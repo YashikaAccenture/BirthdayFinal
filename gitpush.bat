ac990a667e3535af8c45da3f6d391e60c7b58bfd
YashikaAccenture <47723309+YashikaAccenture@users.noreply.github.com>
2019-07-02 18:02:33 +0530

    wgitwatch autoCommit 2019 0Tu.180233

--- gitpush.bat
+++ gitpush.bat
@@ -3,4 +3,3 @@ cd C:\Users\yashika.a.gupta\Desktop\Birthday
 git add --all
 git commit -m "wgitwatch autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push origin master
-exit
