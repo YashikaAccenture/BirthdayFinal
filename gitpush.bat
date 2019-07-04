014d6329871e3c6a587ae0eb04889c838ea274ef
YashikaAccenture <47723309+YashikaAccenture@users.noreply.github.com>
2019-07-04 09:32:27 +0530

    wgitwatch autoCommit 2019 0Th. 93224

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,8 @@
 
 cd C:\Users\yashika.a.gupta\Desktop\WishingTool\Birthday
-git pull https://github.com/YashikaAccenture/BirthdayFinal.git master
+
 git add --all
 git commit -m "wgitwatch autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-git push origin master
+git pull https://github.com/YashikaAccenture/BirthdayFinal.git master
+git push -f origin master
 exit
